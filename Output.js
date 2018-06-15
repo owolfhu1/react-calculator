93cc773f7308dbdef9b94ad95a29f851672e7732
owolfhu1 <owolfhu1@gmail.com>
2018-06-15 15:27:53 -0400

    Update Output.js

--- Output.js
+++ Output.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 
 export default class Output extends Component {
-    
-    constructor() {
-        super();
-        this.state = {
-            value : 'welcome'
-        };
-    }
-    
-    
     render() {
         return (
-            <h1>{this.state.value}</h1>
+            <h2>{this.props.value}</h2>
         );
     }
 }
