104d33ff18181995ce40853633a0459df0ba372a
owolfhu1 <owolfhu1@gmail.com>
2018-06-15 15:27:30 -0400

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,43 +1,67 @@
 import React, { Component } from 'react';
+import cal, { evaluateInfix } from 'calculator-lib';
+//to use calculator lib -> npm install calculator-lib
 import Output from "./Output";
 import ButtonGrid from "./ButtonGrid";
 
 export default class Calculator extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            output: ''
+        };
+    }
+
+    click(item) {
+        let output = this.state.output;
+        if (item === '=')
+            output = evaluateInfix(output);
+        else if (item === "C")
+            output = '';
+        else if (item === "<-")
+            output = output.substr(0, output.length -1);
+        else
+            output += item;
+        this.setState({output: output});
+    };
+
     render() {
         return (
-            <div class="calculator">
-               <Output/>
-                <ButtonGrid/>
+            <div className="calculator">
+                <Output value={this.state.output}/>
+                <ButtonGrid click={this.click.bind(this)}/>
             </div>
         );
     }
+
 }
 
 
 /*
 
-h1 {
+h2 {
     border: black solid 1px;
     margin: 10px;
+    height: 28px;
+    border-radius: 6px;
+    background: white;
 }
-
 .calculator {
     width: 200px;
-    height: 300px;
+    height: 280px;
     margin: auto;
-    border: black solid 2px;
+    border: #898989 solid 2px;
     border-radius: 6px;
+    background: black;
 }
-
 table {
-    border: black solid 1px;
     margin: 10px 10px 10px 10px;
 }
-
 button {
     height: 40px;
     width: 40px;
+    border-radius: 6px;
 }
-
 
  */
