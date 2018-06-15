350356a63833e6ffcf623f7fa0a99d51f6a4d5af
owolfhu1 <owolfhu1@gmail.com>
2018-06-15 15:28:49 -0400

    Update ButtonGrid.js

--- ButtonGrid.js
+++ ButtonGrid.js
@@ -5,11 +5,11 @@ export default class ButtonGrid extends Component {
     render() {
         return (
             <table>
-                <ButtonRow values={['CE', 'C', '<-', '/']}/>
-                <ButtonRow values={['7', '8', '9', '*']}/>
-                <ButtonRow values={['4', '5', '6', '-']}/>
-                <ButtonRow values={['1', '2', '3', '+']}/>
-                <ButtonRow values={['', '0', '.', '=']}/>
+                <ButtonRow click={this.props.click} values={['(', ')', '<-', '/']}/>
+                <ButtonRow click={this.props.click} values={['7', '8', '9', '*']}/>
+                <ButtonRow click={this.props.click} values={['4', '5', '6', '-']}/>
+                <ButtonRow click={this.props.click} values={['1', '2', '3', '+']}/>
+                <ButtonRow click={this.props.click} values={['C', '0', '.', '=']}/>
             </table>
         );
     }
